ad2cd0dcaba9c001b655ef2ffdd0cea1cc6c49da
Paul Lopez <nik.lopez381@gmail.com>
2024-12-08 20:26:50 -0500

    Fixed Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,21 @@
 FROM ubuntu:22.04
 
-RUN apt-get update && apt-get install -y \
+ENV DEBIAN_FRONTEND=noninteractive
+ENV TZ=UTC
+
+# Add the command to ignore release date validation completely
+RUN echo 'Acquire::Check-Valid-Until "0";' > /etc/apt/apt.conf.d/99verify-peer.conf && \
+    echo 'Acquire::Check-Date "0";' >> /etc/apt/apt.conf.d/99verify-peer.conf
+
+# Update without checking timestamps
+RUN apt-get -o Acquire::Check-Valid-Until=false update && \
+    apt-get -o Acquire::Check-Valid-Until=false install -y \
     build-essential \
     cmake \
     python3.10 \
     python3-pip \
+    python3-dev \
+    libffi-dev \
     curl \
     libgtest-dev \
     ninja-build
@@ -15,5 +26,15 @@ ENV PATH="/root/.local/bin:$PATH"
 WORKDIR /app
 COPY . .
 RUN chmod +x scripts/*
+
+# Install Python dependencies explicitly
+RUN pip3 install cffi pytest
+
+# Make sure poetry uses the virtualenv in the container
+ENV POETRY_VIRTUALENVS_IN_PROJECT=true
+ENV POETRY_VIRTUALENVS_CREATE=true
+
+# Install poetry dependencies
+RUN cd /app && poetry install
 
 CMD ["./scripts/build.sh"]
--- scripts/docker-run.sh
+++ scripts/docker-run.sh
@@ -2,22 +2,25 @@
 case $1 in
   build)
     docker build -t quant-fin .
-    docker run -v $(pwd):/app quant-fin ./scripts/build.sh
+    # Clean the build directory first
+    rm -rf build/
+    # Now run with the correct paths
+    docker run -v $(pwd):/app quant-fin bash -c "cd /app && ./scripts/build.sh"
     ;;
   test)
-    docker run -v $(pwd):/app quant-fin ./scripts/run-tests.sh
+    docker run -v $(pwd):/app quant-fin bash -c "cd /app && ./scripts/run-tests.sh"
     ;;
   clean)
-    docker run -v $(pwd):/app quant-fin bash -c "rm -rf build/ && find . -type d -name __pycache__ -exec rm -r {} +"
+    docker run -v $(pwd):/app quant-fin bash -c "cd /app && rm -rf build/ && find . -type d -name **pycache** -exec rm -r {} +"
     docker system prune -f
     ;;
   run)
     case $2 in
       cpp)
-        docker run -v $(pwd):/app quant-fin bash -c "cmake -B build -G Ninja && cmake --build build && ./build/quant_fin_main"
+        docker run -v $(pwd):/app quant-fin bash -c "cd /app && cmake -B build -G Ninja && cmake --build build && ./build/quant_fin_main"
         ;;
       python)
-        docker run -v $(pwd):/app quant-fin poetry run python python_bindings/main.py
+        docker run -v $(pwd):/app quant-fin bash -c "cd /app && poetry run python python_bindings/main.py"
         ;;
       *)
         echo "Usage: ./docker-run.sh run [cpp|python]"
