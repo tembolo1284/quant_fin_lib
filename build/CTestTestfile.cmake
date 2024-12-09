e4d8b3921201d844e62d9a75316cda19959d43b1
Paul Lopez <nik.lopez381@gmail.com>
2024-12-08 20:28:48 -0500

    removed build dir

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /app
-# Build directory: /app/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_quant_fin_lib "/app/build/test_quant_fin_lib")
-set_tests_properties(test_quant_fin_lib PROPERTIES  _BACKTRACE_TRIPLES "/app/CMakeLists.txt;61;add_test;/app/CMakeLists.txt;0;")
