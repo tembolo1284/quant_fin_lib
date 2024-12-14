a9286bc78d0c51161f89ff34d584a830a55e4fcf
Paul Lopez <nik.lopez381@gmail.com>
2024-12-14 11:29:11 -0500

    Updated ci.yml file for github actions error.

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /app
+# Build directory: /app/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_quant_fin_lib "/app/build/bin/test_quant_fin_lib")
+set_tests_properties(test_quant_fin_lib PROPERTIES  WORKING_DIRECTORY "/app/build/bin" _BACKTRACE_TRIPLES "/app/CMakeLists.txt;150;add_test;/app/CMakeLists.txt;0;")
